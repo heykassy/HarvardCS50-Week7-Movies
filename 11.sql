aaf939fce60de4433293e2439cf884e7b7790558
Kamila <101601747+heykassy@users.noreply.github.com>
2022-08-20 21:06:43 -0300

    Add files via upload

---
+++ 11.sql
@@ -0,0 +1,7 @@
+--In 11.sql, write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
+--Your query should output a table with a single column for the title of each movie.
+--You may assume that there is only one person in the database with the name Chadwick Boseman.
+SELECT movies.title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id JOIN ratings ON movies.id = ratings.movie_id WHERE people.name = "Chadwick Boseman" ORDER by ratings.rating DESC LIMIT 5;
+
+--PS: Tried using nested queries but CHECK50 kept saying the output was incorrect, even though on my terminal it was correct. Runtime is significantly different.
+--SELECT title FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER by rating DESC LIMIT 5);
